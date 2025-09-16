a00446f775268753e393b386074e2abb8adca6c4
dradam124 <nagykovaadam@gmail.com>
2025-09-16 11:01:09 +0200

    fein2

---
+++ eso.py
@@ -0,0 +1,23 @@
+import random
+
+szamok=[]
+
+# 100 db elmü lista feltötése 1-99 közt
+
+
+for i in range(100):
+
+#..... szam generalas 
+   
+    rszam = random.randint(1, 100)
+
+# szam elhelyezése 
+   
+    szamok.append(rszam)
+    
+print(szamok)
+
+jatek_szam=0
+nemndb=0
+
+kszam= random.szamok
